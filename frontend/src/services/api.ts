7594862dec1d5e08bb7a4703c959e05eb2f1e7a3
ozgurcanyilmaz <yilmazozgur190@gmail.com>
2025-11-17 10:50:34 +0300

    token süresi dolunca logine atma eklendi

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,18 +1,29 @@
-import axios from 'axios'
+import axios from "axios";
+import { logout } from "./auth";
 
-export const API_BASE_URL = 'http://localhost:5180'
+export const API_BASE_URL = "http://localhost:5180";
 
 export const api = axios.create({
   baseURL: `${API_BASE_URL}/api`,
-})
+});
 
 api.interceptors.request.use((config) => {
-  const token = localStorage.getItem('token')
+  const token = localStorage.getItem("token");
   if (token) {
-    config.headers = config.headers || {}
-    config.headers.Authorization = `Bearer ${token}`
+    config.headers = config.headers || {};
+    config.headers.Authorization = `Bearer ${token}`;
   }
-  return config
-})
+  return config;
+});
 
-
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response && error.response.status === 401) {
+      logout();
+      window.location.replace("/login");
+      return Promise.reject(error);
+    }
+    return Promise.reject(error);
+  }
+);
